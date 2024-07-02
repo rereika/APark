1f95dabeed3954e149c38df274c97864860ea07f
rereika <rere970807@gmail.com>
2024-07-02 09:21:36 +0900

    feat: ホーム画面の修正

--- laravel-project/resources/views/APark/home.blade.php
+++ laravel-project/resources/views/APark/home.blade.php
@@ -6,37 +6,62 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>APark</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+  <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-6/css/6-1-6.css">
+<link rel="stylesheet" href="{{ asset('css/common.css') }}">
   <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 </head>
 
 <body>
 
   <header>
-    <div class="icon">
-    <img src="{{ asset('image/logo.png') }}" alt="ロゴ画像">
+
+    <div class="logo">
+      <img src="{{ asset('image/logo.png') }}" alt="ロゴ画像">
     </div>
 
-    <a href="#" class="my_page">マイページ</a>
-    <a href="#" class="create_idea">作成する</a>
+    <div class="home-menu">
+      <ul>
+        <li><a href="#" class="home_menu">マイページ</a></li>
+        <li><a href="#" class="home_menu">作成する</a></li>
+      </ul>
+    </div>
+
   </header>
 
   <div class="catch_copy">
-    <img src="" alt="">
+    <p>アイデアの補助輪に乗り、アプレンティスシップの旅に出よう</p>
   </div>
 
-  <div class="inner">
+    <div class="new-ideas-box-animate">
+      <ul class="slider">
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+        <li><img src="{{ asset('image/sample_chart.png') }}" alt="サンプルチャート画像">
+        </li>
+      </ul>
+    </div>
 
-  <div class="sort_bytheme">
-    <p>テーマで並び替える</p>
-      <button type="button" class="">チーム開発 DEV1</button>
-      <button type="button" class="">オリジナルプロダクト</button>
-      <button type="button" class="">チーム開発 DEV2</button>
-  </div>
 
-    <p>新着のアイデア</p>
-    <div class="new_posts"></div>
-  </div>
+    <div class="sort_bytheme">
+        <button type="button" class="">チーム開発 DEV1</button>
+        <button type="button" class="">オリジナルプロダクト</button>
+        <button type="button" class="">チーム開発 DEV2</button>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-6/js/6-1-6.js"></script>
+
 </body>
 
 </html>
