ae30b68a40bb083d635cbfdb3a58c83307bbccde
rereika <rere970807@gmail.com>
2024-07-24 13:47:47 +0900

    error: ホーム画面にチャートを表示

--- laravel-project/public/js/home.js
+++ laravel-project/public/js/home.js
@@ -95,13 +95,13 @@ let radarConfig = {
     labels: ['類いない', '使用技術の正確性', '目新しさ', 'ストーリー性', 'わくわく'],
     datasets: [{
       label: 'Self',
-      data: [0, 0, 0, 0, 0],
+      data: selfValues,
       backgroundColor: 'rgba(255, 136, 136, 0.3)',  // 赤色の透明な背景色
       borderColor: 'rgb(255, 136, 136)',  // 赤色の境界線
       borderWidth: 5
     }, {
       label: 'FB',
-      data: [0, 0, 0, 0, 0],
+      data: fbValues,
       backgroundColor: 'rgba(54, 162, 235, 0.2)',  // 青色の透明な背景色
       borderColor: 'rgba(54, 162, 235, 1)',  // 青色の境界線
       borderWidth: 5
@@ -148,18 +148,13 @@ let radarConfig = {
   }
 };
 
-let RadarCtx = document.getElementsByName('feedBackRadarChart');
-
-RadarCtx.forEach((r) => radarChart = new Chart(r, radarConfig));
 
 // let radarChart = new Chart(RadarCtx, radarConfig);
 
 // チャートの更新
-function feedBackUpdateChart() {
-  let fbForm = document.getElementsByName('feedBackChartForm');
-
-  fbForm.forEach((f) => fbFormData = new FormData(f, fbForm));
-  // let fbFormData = new FormData(fbForm);
+function feedBackUpdateChart(radarChart, index) {
+  let fbForm = document.getElementByName('feedBackChartForm')[index];
+  let fbFormData = new FormData(fbForm);
 
   let selfValues = [
     fbFormData.get('self_chart1'),
@@ -182,5 +177,12 @@ function feedBackUpdateChart() {
   radarChart.update();
 }
 
-window.addEventListener('load', feedBackUpdateChart);
+let radarCharts = [];
+document.getElementsByName('feedBackRadarChart').forEach((ctx) => radarCharts.push(new Chart(ctx, radarConfig)));
+
+// window.addEventListener('load', feedBackUpdateChart);
+window.addEventListener('load', () => radarCharts.forEach((chart, index) => feedBackUpdateChart(chart, index)));
 document.getElementById('feedBackChartForm').addEventListener('change', feedBackUpdateChart);
+
+// window.addEventListener('load', feedBackUpdateChart);
+// document.getElementById('feedBackChartForm').addEventListener('change', feedBackUpdateChart);
